d2ca76a3f1bc681441655643dea380f0cfcf39d2
Guy Roberts <guyroberts21@outlook.com>
2020-08-18 22:26:00 +0100

    Updates

---
+++ precache-manifest.34abdf46d396220a940cc3f5b7ee63bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efcf7a3e80c113c55c41a5ceff868b0",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "cbb6809fd2326d31a9a8",
+    "url": "/battleship/static/css/main.cea5b5b3.chunk.css"
+  },
+  {
+    "revision": "162550ef79a70a8c746c",
+    "url": "/battleship/static/js/2.2b3d89a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.2b3d89a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb6809fd2326d31a9a8",
+    "url": "/battleship/static/js/main.f35faf70.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
